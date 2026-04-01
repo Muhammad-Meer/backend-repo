9e1af64c6d04892ac7fcb0f2ff49a310aebf3be9
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-01 13:28:34 +0500

    push creaateaccountcontrollerh

--- src/controller/account.controller.js
+++ src/controller/account.controller.js
@@ -2,3 +2,16 @@ const accountmodel = require('../models/account.model')
 
 
 
+async function creaateaccountcontroller(req , res) {
+
+    const user =  req.user
+
+    const account = await accountmodel.create({user : user._id})
+    res.status(201).json({
+        message : "account created successfully",
+        account : account
+    })
+}
+
+
+module.exports = {creaateaccountcontroller}
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -2,9 +2,24 @@ const express = require('express')
 
 
 
+const authmiddleware = require('../middleware/auth.middleware')
+const creaateaccountcontroller = require('../controller/account.controller')
+
+
+
+
 
 
 const router = express.Router()
+
+
+
+// Post/ api/ accounts
+// create a new account
+// proteected route
+
+
+router.post("/", authmiddleware.authmiddleware, creaateaccountcontroller.creaateaccountcontroller)
 
 
 
