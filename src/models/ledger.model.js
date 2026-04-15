74069291692f086b9e027759c9eb4fd8fc0c737c
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-15 14:12:58 +0500

    newcode

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const trancactionmodel = require('./transaction.model');
 
 
 const ledgerschema = new mongoose.Schema({
@@ -14,9 +15,32 @@ const ledgerschema = new mongoose.Schema({
     type: Number,
     required: [true, "amount is required for a create ledger system"],
     immutable: true,
+  },
+
+  trancactio: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "transaction",
+    required: [true, "amount is required for a create ledger system"],
+    immutable: true,
+  },
+  type: {
+    typr: string,
+
+    enum: {
+      values: ["DEBIT", "CREDIT"],
+      message: "type can be either DEBIT or CREDIT",
+    },
+    required: [true, "ledger type is required"],
+    immutable: true,    
   }
 })
 
 
 
 
+
+
+const ledgermodel = mongoose.model("ledger", ledgerschema)
+
+
+module.exports = ledgermodel
