bead2b4df5721fe6c0ebba365e24200aff9a3b5e
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-15 13:39:30 +0500

    fix

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -8,6 +8,12 @@ const ledgerschema = new mongoose.Schema({
     required: [true, "ledger must be associated with an account"],
     required: true,
     index: true,
+    immutable: true,
+  },
+  amount: {
+    type: Number,
+    required: [true, "amount is required for a create ledger system"],
+    immutable: true,
   }
 })
 
