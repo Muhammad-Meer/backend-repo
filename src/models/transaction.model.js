cb9908fcbb5c113cf0709e21088269404e2d0807
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-15 13:01:01 +0500

    fix some issue

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -15,5 +15,24 @@ const transactionschema = new mongoose.Schema({
     required: [true , "transaction must be associated with an account"],
     required: true,
     index: true,
-   }
+   },
+   status: {
+    type: String,
+    enum: {
+    values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+    message: "status can be either PENDING, COMPLETED, FAILED or REVERSED",
+    },
+     dafult: "PENDING"
+   },
+
+    amount: {
+      type: Number,
+      required: [true , "amount is required for a transaction"],
+        min: [0.01, "amount must be greater then zero"],
+    },
+
+    idempotencykey: {
+      type: String,
+    }
+
 })
