0eb96471290fdaa0e6f471088db22d52ea9f5211
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-15 13:15:30 +0500

    newcode

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -2,37 +2,47 @@ const mongoose = require('mongoose');
 
 
 const transactionschema = new mongoose.Schema({
-   account: {
+  account: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "account",
-    required: [true , "transaction must be associated with an account"],
+    required: [true, "transaction must be associated with an account"],
     required: true,
     index: true,
-   },
-   toaccount: {
+  },
+  toaccount: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "account",
-    required: [true , "transaction must be associated with an account"],
+    required: [true, "transaction must be associated with an account"],
     required: true,
     index: true,
-   },
-   status: {
+  },
+  status: {
     type: String,
     enum: {
-    values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
-    message: "status can be either PENDING, COMPLETED, FAILED or REVERSED",
+      values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+      message: "status can be either PENDING, COMPLETED, FAILED or REVERSED",
     },
-     dafult: "PENDING"
-   },
+    dafult: "PENDING"
+  },
 
-    amount: {
-      type: Number,
-      required: [true , "amount is required for a transaction"],
-        min: [0.01, "amount must be greater then zero"],
-    },
+  amount: {
+    type: Number,
+    required: [true, "amount is required for a transaction"],
+    min: [0.01, "amount must be greater then zero"],
+  },
 
-    idempotencykey: {
-      type: String,
-    }
+  idempotencykey: {
+    type: String,
+    required: [true, "idempotency key is required for a transaction"],
+    unique: true,
+    index: true,
+  }
+},
+  { timestamps: true }
+)
 
-})
+
+const trancactionmodel = mongoose.model("transaction", transactionschema)
+
+
+module.exports = trancactionmodel
