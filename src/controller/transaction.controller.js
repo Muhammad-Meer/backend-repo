ee6b4bb6e9f28617f2ce71aa9a8eac16239feedb
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-18 10:27:13 +0500

    newcode

--- src/controller/transaction.controller.js
+++ src/controller/transaction.controller.js
@@ -7,67 +7,72 @@ const accountmodel = require('../models/account.model')
 async function createTransaction(req, res) {
 
   const { fromAccount, toAccount, amount, idempotencykey } = req.body;
- 
-  if(!fromAccount  ||   !toAccount  ||  !amount||   !idempotencykey ) {
+
+  if (!fromAccount || !toAccount || !amount || !idempotencykey) {
     return res.status(400).json({
-      message : "every field is requiresd"
+      message: "every field is requiresd"
     })
-  } 
+  }
 
   const fromAccountuser = await accountmodel.findOne({
     _id: fromAccount
-})
-
-
-  const toAccountuser = await  accountmodel.findOne({
-    _id:  toAccount
   })
 
-  if(!fromAccountuser || !toAccountuser) {
+
+  const toAccountuser = await accountmodel.findOne({
+    _id: toAccount
+  })
+
+  if (!fromAccountuser || !toAccountuser) {
     return res.status(400).json({
-    message: " Invalid accounts"
-    })}
+      message: " Invalid accounts"
+    })
+  }
 
 
-   const isTransactionAlreadyExist = await trancactionmodel.findOne({
+  const isTransactionAlreadyExist = await trancactionmodel.findOne({
     idempotencykey: idempotencykey
-   })
+  })
 
-   if(isTransactionAlreadyExist) {
+  if (isTransactionAlreadyExist) {
 
-    if(isTransactionAlreadyExist.status === "COMPLETED") {
+    if (isTransactionAlreadyExist.status === "COMPLETED") {
 
-          return res.status(200).json({
-          message: "Transaction already completed with this idempotency key"
+      return res.status(200).json({
+        message: "Transaction already completed with this idempotency key"
+      })
+
+    }
+
+    if (isTransactionAlreadyExist.status === "PENDING") {
+      return res.status(200).json({
+        message: "transaction is still processing"
+      })
+    }
+
+
+    if (isTransactionAlreadyExist.status === "FAILED") {
+      return res.status(500).json({
+        message: "transaction failed previously , please try again"
+      })
+    }
+
+    if (isTransactionAlreadyExist.status === "REVERSED") {
+      return res.status(400).json({
+        message: "transaction was reversed , please try again"
+      })
+    }
+
+  }
+
+  if (fromAccountuser.status !== "ACTIVE" || toAccountuser.status !== "ACTIVE") {
+
+    return res.status(400).json({
+      message: "both accounts must be active   or one of them is not active"
     })
 
-    }
-
-    if(isTransactionAlreadyExist.status === "PENDING") {
-          return res.status(200).json({
-          message : "transaction is still processing"
-      })
-    }
-
-    
-        if(isTransactionAlreadyExist.status === "FAILED") {
-          return res.status(500).json({
-          message : "transaction failed previously , please try again"
-      })
-     }
-
-             if(isTransactionAlreadyExist.status === "REVERSED") {
-          return res.status(400).json({
-          message : "transaction was reversed , please try again"
-      })
-    }
-   
-} 
-
-if(fromAccountuser.status !== "ACTIVE" || toAccountuser.status !== "ACTIVE") {
+  }
 
 }
 
-}
-
-module.exports = {createTransaction}
+module.exports = { createTransaction }
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,31 +1,44 @@
 const mongoose = require('mongoose')
+const ledgermodel = require('./ledger.model/')
 
 const accountschemma = new mongoose.Schema({
 
    user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: [true , "Account is must be assoicated with a user" ],
-    index: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "Account is must be assoicated with a user"],
+      index: true
    },
 
-status: {
-  type: String,
-  enum: ["ACTIVE", "FROZEN", "CLOSED"],
-  default: "ACTIVE"
-},
+   status: {
+      type: String,
+      enum: ["ACTIVE", "FROZEN", "CLOSED"],
+      default: "ACTIVE"
+   },
 
    currency: {
-    type: String,
-    required:  [true , "currency is required for creating an account"],
-    default: "PKR"   }
+      type: String,
+      required: [true, "currency is required for creating an account"],
+      default: "PKR"
+   }
 
 },
-     {timestamps: true
+   {
+      timestamps: true 
+   })
 
-})
+accountschemma.index({ user: 1, status: 1 })
 
-accountschemma.index({user: 1, status: 1})
+accountschemma.methods.getBalance = async function () {
+
+   const Balance = await ledgermodel.aggregate([
+      {
+         $match: {
+            acco
+         }
+      }
+   ])
+}
 
 
 const accountmodel = mongoose.model("account", accountschemma)
